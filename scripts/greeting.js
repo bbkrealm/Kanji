a0f5f09d358670350d1d0a80c03bb053d9aaa8d5
Alededorigo <alededorigo@gmail.com>
2020-12-31 13:16:20 +0100

    Update

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,25 +1,20 @@
-// Get the hour
 var today = new Date();
 var hour = today.getHours();
 
-// Here you can change your name
-var name = 'Dedo';
-
-// Here you can change your greetings
-var gree1 = 'おやすみなさい ';
-var gree2 = 'おはようございます ';
-var gree3 = 'こんにちは ';
-var gree4 = 'こんばんは ';
-var gree5 = 'こんばんは ';
-var gree6 = 'こんばんは ';
+var g1 = 'おやすみなさい ';
+var g2 = 'おはようございます ';
+var g3 = 'こんにちは ';
+var g4 = 'こんばんは ';
+var g5 = 'こんばんは ';
+var g6 = 'こんばんは ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
-    document.getElementById('greetings').innerText = gree1;
+    document.getElementById('greetings').innerText = g1;
 } else if (hour >= 6 && hour < 12) {
-    document.getElementById('greetings').innerText = gree2;
+    document.getElementById('greetings').innerText = g2;
 } else if (hour >= 12 && hour < 17) {
-    document.getElementById('greetings').innerText = gree3;
+    document.getElementById('greetings').innerText = g3;
 } else  {
-    document.getElementById('greetings').innerText = gree4;
+    document.getElementById('greetings').innerText = g4;
 }
